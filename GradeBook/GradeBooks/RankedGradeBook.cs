f4867b49b0d1ac4963969d59b5db3cf09bd4fa2b
Khanguye <trongkha@yahoo.com>
2020-01-01 12:25:52 -0800

    Grade Ranked Logic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook  
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name) {
+        public RankedGradeBook(string name) : base(name)
+        {
             Type = Enums.GradeBookType.Ranked;
         }
 
@@ -17,9 +19,28 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
 
-           
+            var numStdIn20 = (int)Math.Ceiling(Students.Count * 0.2);
+            var stdSortAvgGrades = Students.OrderByDescending(s => s.AverageGrade).Select(g => g.AverageGrade).ToList();
+
+            if (stdSortAvgGrades.Skip(numStdIn20 - 1).Take(1).First() < averageGrade)
+            {
+                return 'A';
+            }
+            else if (stdSortAvgGrades.Skip(numStdIn20 * 2 - 1).Take(1).First() < averageGrade)
+            {
+                return 'B';
+            }
+            else if (stdSortAvgGrades.Skip(numStdIn20 * 3 - 1).Take(1).First() < averageGrade)
+            {
+                return 'C';
+            }
+            else if (stdSortAvgGrades.Skip(numStdIn20 * 4 - 1).Take(1).First() < averageGrade)
+            {
+                return 'D';
+            }
 
             return 'F';
+
         }
     }
 }
