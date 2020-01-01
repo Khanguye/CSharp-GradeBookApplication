f65b7697873babd175448db3a880d3b3789965ec
Khanguye <trongkha@yahoo.com>
2019-12-31 22:53:01 -0800

    GradeBookType
    Create GradeBookType
    New property Type of GradeBookType

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,8 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        GradeBookType Type { get; set; }
+
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
